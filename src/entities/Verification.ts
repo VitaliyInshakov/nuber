78282a72ba50f93cd8aff782a073b9376b34e779
Vitaliy Inshakov <michael8913@gmail.com>
2019-12-10 23:03:52 +0200

    added user and verification entities

---
+++ src/entities/Verification.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  BaseEntity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+class Verification extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column({type: "text"})
+  target: string;
+
+  @Column({type: "text"})
+  payload: string;
+
+  @Column({type: "text"})
+  key: string;
+
+  @Column({type: "boolean", default: false})
+  used: boolean;
+  
+  @CreateDateColumn() createdAt: string;
+  @UpdateDateColumn() updatedAt: string;
+}
