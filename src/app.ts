4ab2c2224cb9623db793a4636202cee4aa861df4
Vitaliy Inshakov <michael8913@gmail.com>
2020-01-14 22:59:57 +0200

    added resolvers GetNearbyDrivers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Response } from "express";
 import cors  from "cors";
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import helmet from "helmet";
 import logger from "morgan";
 import schema from "./schema";
@@ -8,8 +8,11 @@ import decodeJWT from "./utils/decodeJWT";
 
 class App {
   public app: GraphQLServer;
+  public pubSub: any;
 
   constructor() {
+    this.pubSub = new PubSub();
+    this.pubSub.ee.setMaxListeners(99);
       this.app = new GraphQLServer({
         schema,
         context: req => {
