6d25e32f1b041f5ad7cdad681dd24da471668962
Vitaliy Inshakov <michael8913@gmail.com>
2020-01-19 22:35:02 +0200

    implement subscriptions

--- src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
+++ src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
@@ -1,9 +1,29 @@
+import { withFilter } from "graphql-yoga";
+import User from "../../../entities/User";
+
 const resolvers = {
     Subscription: {
         DriversSubscription: {
-            subscribe: (_, __, { pubsub }) => {
-                return pubsub.asyncIterator("driverUpdate")
-            }
+            subscribe: withFilter(
+                (_, __, { pubsub }) => pubsub.asyncIterator("driverUpdate"),
+                (payload, _, { context }) => {
+                    const user: User = context.currentUser;
+                    const {
+                        DriversSubscription: {
+                            lastLat: driverLastLat,
+                            lastLng: driverLastLng,
+                        }
+                    } = payload;
+
+                    const { lastLng: userLastLng, lastLat: userLastLat } = user;
+                    return (
+                        driverLastLat >= userLastLat - 0.05
+                        && driverLastLat <= userLastLat + 0.05
+                        && driverLastLng >= userLastLng - 0.05
+                        &&driverLastLng <= userLastLng + 0.05
+                    )
+                },
+            ),
         }
     }
 };
