1497de2fdb6ba301f3c5f2c1071777da15bfcfb2
Vitaliy <michael8913@gmail.com>
2020-02-23 22:43:52 +0200

    added react toastify

--- client/src/Components/App/Container.tsx
+++ client/src/Components/App/Container.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { graphql } from "react-apollo";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 import GlobalStyle from "../../global-styles";
 import theme from "../../theme";
@@ -9,10 +11,13 @@ import Presenter from "./Presenter";
 import { IS_LOGGED_IN } from "./Queries";
 
 const Container = ({ data }: { data?: any;}) => (
-    <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Presenter isLoggedIn={data.auth.isLoggedIn}/>
-    </ThemeProvider>
+    <>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Presenter isLoggedIn={data.auth.isLoggedIn}/>
+        </ThemeProvider>
+        <ToastContainer draggable={true} position={"bottom-center"}/>
+    </>
 );
 
 export default graphql(IS_LOGGED_IN)(Container);
--- client/src/Routes/PhoneLogin/Container.tsx
+++ client/src/Routes/PhoneLogin/Container.tsx
@@ -1,9 +1,50 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { toast } from "react-toastify";
+
 import Presenter from "./Presenter";
 
-class PhoneLoginContainer extends React.Component {
+interface IState {
+    countryCode: string;
+    phoneNumber: string;
+}
+
+class PhoneLoginContainer extends React.Component<RouteComponentProps<any>, IState> {
+    public state = {
+        countryCode: "+82",
+        phoneNumber: "",
+    };
+
+    public onInputChange: React.ChangeEventHandler<HTMLInputElement | HTMLSelectElement> = (event) => {
+        const { target: { name, value } } = event;
+
+        this.setState({
+            [name]: value,
+        } as any);
+    };
+
+    public onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+        const { countryCode, phoneNumber } = this.state;
+
+        const phone = `${countryCode}${phoneNumber}`;
+        const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(phone);
+
+        if (isValid) {
+
+        } else {
+            toast.error("Please write a valid phone number");
+        }
+    };
+
     public render() {
-        return <Presenter />;
+        const { countryCode, phoneNumber } = this.state;
+        return <Presenter
+            countryCode={countryCode}
+            phoneNumber={phoneNumber}
+            onInputChange={this.onInputChange}
+            onSubmit={this.onSubmit}
+        />;
     }
 }
 
