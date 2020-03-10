61d83f9827ee7040ce1267a38855ef7bf0ee4582
Vitaliy <michael8913@gmail.com>
2020-03-10 23:10:48 +0200

    Update profile p.2-3

--- client/src/Routes/EditAccount/Container.tsx
+++ client/src/Routes/EditAccount/Container.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
-import { useMutation } from "@apollo/react-hooks"
+import { toast } from "react-toastify";
+import { useMutation, useQuery } from "@apollo/react-hooks"
 
+import { USER_PROFILE } from "../../sharedQueries";
 import Presenter from "./Presenter";
 import { UPDATE_PROFILE } from "./Queries";
-import { updateProfile, updateProfileVariables } from "../../types/api";
+import { updateProfile, updateProfileVariables, userProfile } from "../../types/api";
 
 interface IState {
     firstName: string;
@@ -21,12 +23,24 @@ const Container: React.FC<RouteComponentProps<any>> = () => {
         profilePhoto: "",
     });
 
+    useQuery<userProfile>(USER_PROFILE, {
+        onCompleted: data => updateFields(data),
+    });
     const [updateProfileFn, { loading }] = useMutation<updateProfile, updateProfileVariables>(UPDATE_PROFILE, {
         variables: {
             email: state.email,
             firstName: state.firstName,
             lastName: state.lastName,
             profilePhoto: state.profilePhoto,
+        },
+        refetchQueries: [{ query: USER_PROFILE }],
+        onCompleted: data => {
+            const { UpdateProfile } = data;
+            if (UpdateProfile!.ok) {
+                toast.success("Profile updated!");
+            } else if (UpdateProfile!.error) {
+                toast.error(UpdateProfile!.error);
+            }
         }
     });
 
@@ -39,8 +53,24 @@ const Container: React.FC<RouteComponentProps<any>> = () => {
         }));
     };
 
-    const { email, firstName, lastName, profilePhoto } = state;
+    const updateFields = (data: {} | userProfile) => {
+        if ("GetMyProfile" in data) {
+            const { GetMyProfile: { user } } = data;
 
+            if (user !== null) {
+                const { firstName, lastName, email, profilePhoto } = user;
+                setState(prevState => ({
+                    ...prevState,
+                    email,
+                    firstName,
+                    lastName,
+                    profilePhoto,
+                } as any));
+            }
+        }
+    };
+
+    const { email, firstName, lastName, profilePhoto } = state;
     return (
         <Presenter
             email={email}
