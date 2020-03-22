85ed7a4b2fdb2e20d408a4a8309f1f551b071614
Vitaliy <michael8913@gmail.com>
2020-03-22 22:37:46 +0200

    Chat screen mutation

--- client/src/Routes/Chat/Container.tsx
+++ client/src/Routes/Chat/Container.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
 import { USER_PROFILE } from "../../sharedQueries";
-import { getChat, getChatVariables, userProfile } from "../../types/api";
+import {
+    getChat,
+    getChatVariables,
+    userProfile,
+    sendMessage,
+    sendMessageVariables,
+} from "../../types/api";
 
 import Presenter from "./Presenter";
-import {GET_CHAT} from "./Queries";
+import { GET_CHAT, SEND_MESSAGE } from "./Queries";
+
+interface IState {
+    message: string;
+}
 
 const Container: React.FC<RouteComponentProps<any>> = (props) => {
+    const [state, setState] = useState<IState>({
+        message: "",
+    });
+
     if (!props.match.params.chatId) {
         props.history.push("/");
     }
@@ -16,10 +30,39 @@ const Container: React.FC<RouteComponentProps<any>> = (props) => {
     const { data: userData } = useQuery<userProfile>(USER_PROFILE);
     const { data, loading } = useQuery<getChat, getChatVariables>(GET_CHAT);
 
+    const [sendMessageFn] = useMutation<sendMessage, sendMessageVariables>(SEND_MESSAGE);
+
+    const onInputChange: React.ChangeEventHandler<HTMLInputElement> = event => {
+        const { target: { name, value } } = event;
+        setState(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const onSubmit = (): void => {
+        const { match: { params: { chatId } } } = props;
+        if (state.message !== "") {
+            setState({
+                message: ""
+            });
+            sendMessageFn({
+                variables: {
+                    chatId,
+                    text: state.message
+                }
+            });
+        }
+        return;
+    };
+
     return <Presenter
         data={data}
         loading={loading}
         userData={userData}
+        messageText={state.message}
+        onInputChange={onInputChange}
+        onSubmit={onSubmit}
     />;
 };
 
--- client/src/Routes/Chat/Presenter.tsx
+++ client/src/Routes/Chat/Presenter.tsx
@@ -3,20 +3,41 @@ import styled from "styled-components";
 
 import Header from "../../Components/Header";
 import Message from "../../Components/Message";
+import Form from "../../Components/Form";
+import Input from "../../Components/Input";
 import { getChat, userProfile } from "../../types/api";
 
 const Container = styled.div``;
+
+const Chat = styled.div`
+  height: 80vh;
+  overflow: scroll;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const InputCont = styled.div`
+  padding: 0 20px;
+`;
 
 interface IProps {
     data?: getChat;
     userData?: userProfile;
     loading: boolean;
+    messageText: string;
+    onSubmit: () => void;
+    onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Presenter: React.FC<IProps> = ({
     loading,
     data: { GetChat: { chat = null } = {} } = {},
-    userData: { GetMyProfile: { user = null } = {} } = {}
+    userData: { GetMyProfile: { user = null } = {} } = {},
+    messageText,
+    onInputChange,
+    onSubmit,
 }) => (
     <Container>
         <Header title={"Chat"} />
@@ -24,19 +45,31 @@ const Presenter: React.FC<IProps> = ({
         chat &&
         user && (
             <>
-                {chat.messages &&
-                chat.messages.map(message => {
-                    if (message) {
-                        return (
-                            <Message
-                                key={message.id}
-                                text={message.text}
-                                mine={user.id === message.userId}
-                            />
-                        );
-                    }
-                    return null;
-                })}
+                <Chat>
+                    {chat.messages &&
+                    chat.messages.map(message => {
+                        if (message) {
+                            return (
+                                <Message
+                                    key={message.id}
+                                    text={message.text}
+                                    mine={user.id === message.userId}
+                                />
+                            );
+                        }
+                        return null;
+                    })}
+                </Chat>
+                <InputCont>
+                    <Form submitFn={onSubmit}>
+                        <Input
+                            value={messageText}
+                            placeholder={"Type your message"}
+                            onChange={onInputChange}
+                            name={"message"}
+                        />
+                    </Form>
+                </InputCont>
             </>
         )}
     </Container>
